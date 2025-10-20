a5aaa70746e23b6e9a4e55410c6288e9b6af09f8
Mihailo Drinic <drinicmihailo@gmail.com>
2025-10-20 08:58:42 +0200

    CUDA

--- GPU/test.cu
+++ GPU/test.cu
@@ -1,5 +1,18 @@
 #include <iostream>
 #include <random>
+#include <cuda_runtime.h>
+
+__global__ void tensorAdd(float* A, float* B, float* C, int rows, int cols) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int size = rows * cols;
+    if(idx < size) {
+        C[idx] = A[idx] + B[idx];
+    };
+}
+
+__global__ void tensorMultiply(float* A, float* B, float*C, int rows, int cols) {
+
+}
 
 class Tensor {
     private:
@@ -8,11 +21,16 @@ class Tensor {
         int cols;
 
     public:
-    Tensor(int nRows, int nCols) {
+    Tensor(int nRows, int nCols, bool random = true) {
         this->rows = nRows;
         this->cols = nCols;
         this->matrix = new float[this->rows * this->cols];
-        init();
+        if(random) {
+            init();
+        }
+        else {
+            initZeros();
+        }
     };
 
     void init() {
@@ -27,20 +45,78 @@ class Tensor {
         }
     }
 
+    void initZeros() {
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                matrix[i * cols + j] = 0.0f;
+            };
+        };
+    };
+
     void print() {
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 std::cout << matrix[i * cols + j] << " ";
             }
             std::cout << "\n";
-        }
+        };
+    };
+
+    int getRows() { return rows; };
+
+    int getCols() { return cols; };
+
+    float* getMatrix() {
+        return matrix;
     }
+
+    Tensor operator+(Tensor& other) const {
+        if(rows == other.rows && cols == other.cols) {
+            Tensor r(rows, cols);
+            size_t size = rows * cols * sizeof(float);
+
+            float *d_A, *d_B, *d_C;
+            cudaMalloc(&d_A, size);
+            cudaMalloc(&d_B, size);
+            cudaMalloc(&d_C, size);
+
+            cudaMemcpy(d_A, matrix, size, cudaMemcpyHostToDevice);
+
+        }
+    };
 };
 
 int main() {
+    Tensor tensorA(3, 3);
+    Tensor tensorB(3, 3);
+    Tensor tensorC(3, 3, false);
 
-    Tensor t(3, 3);
-    t.print();
+    size_t size = tensorA.getRows() * tensorA.getCols() * sizeof(float);
+
+    float *d_A, *d_B, *d_C;
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+
+    cudaMemcpy(d_A, tensorA.getMatrix(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, tensorB.getMatrix(), size, cudaMemcpyHostToDevice);
+
+    int threads = 256;
+    int blocks = (tensorA.getRows() * tensorA.getCols() + threads - 1)/threads;
+    tensorAdd<<<blocks, threads>>>(d_A, d_B, d_C, tensorA.getRows(), tensorA.getCols());
+
+    cudaMemcpy(tensorC.getMatrix(), d_C, size, cudaMemcpyDeviceToHost);
+
+    for(int i = 0; i < tensorA.getRows(); i++) {
+        for(int j = 0; j < tensorA.getCols(); j++) {
+            std::cout << tensorC.getMatrix()[i * tensorA.getCols() + j] << " ";
+        }
+        std::cout << "\n";
+    };
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
 
     return 0;
 }
